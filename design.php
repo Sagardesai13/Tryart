c13afc1be236c04b8a636e511dc3bc499c37ca18
shruti942 <jagtapshruti2610@gmail.com>
2021-09-21 13:42:38 +0530

    chatbot

---
+++ design.php
@@ -0,0 +1,53 @@
+<section class="my-5">
+        <div class="py-5">
+            <div class="pt-5">
+                <h2 class="text-center pt-5">More Ideas</h2>
+                <hr>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img2.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img3.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img28.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img29.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img46.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img45.jpg" class="img-fluid pb-4">
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img44.jpg" class="img-fluid pb-4">
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img43.jpg" class="img-fluid pb-4">
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img42.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img47.jpg" class="img-fluid pb-4">
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img25.jpg" class="img-fluid pb-4">
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <img src="image/img48.jpg" class="img-fluid pb-4">
+                </div>
+            </div>
+        </div>
+
+    </section>
