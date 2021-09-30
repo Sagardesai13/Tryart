c7f39b71dcd3442ecde20b3a116c00bef0555ada
shruti942 <jagtapshruti2610@gmail.com>
2021-09-30 18:59:50 +0530

    help

---
+++ wall/help.php
@@ -0,0 +1,80 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet"
+        id="bootstrap-css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+
+    <title>Tryart</title>
+</head>
+
+<section class="my-2">
+    <div class="py-2">
+        <h2 class="text-center" id="aboutus">Help
+            <hr class="width:60% margin-left:auto margin-right:auto" />
+        </h2>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-10">
+                <div class=" aboutimg pb-5"><img src="Designs/img2.jpg" class="img-fluid "></div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-10">
+                <h2>Select Design</h2>
+                <p class="py-3">Click on select design button. And then you can select the designs from your device and also you can select the designs from our design ideas.
+            </div>
+
+                <div class="col-lg-2 col-md-2 col-10">
+                    <div class=" aboutimg pb-5"><img src="Designs/wallimg.jpeg" class="img-fluid "></div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-10">
+                    <h2>Select Wall</h2>
+                    <p class="py-3"> Click on select image button.You can choose plan white wall or any image from your device.
+                </div>
+
+                                <div class="col-lg-2 col-md-2 col-10">
+                                    <div class=" aboutimg pb-5"><img src="Designs/img4.jpg" class="img-fluid "></div>
+                                </div>
+
+                                <div class="col-lg-4 col-md-4 col-10">
+                                    <h2>Click make my Wall</h2>
+                                    <p class="py-3">Hit the Make your wall button.You'll be able to see your selected choices after this.
+                                </div>
+
+                                                <div class="col-lg-2 col-md-2 col-10">
+                                                    <div class=" aboutimg pb-5"><img src="Designs/img3.jpg" class="img-fluid "></div>
+                                                </div>
+
+                                                <div class="col-lg-4 col-md-4 col-10">
+                                                    <h2>Select area</h2>
+                                                    <p class="py-3"> Select the area of wall where you want to apply design and you'll able to see ruff idea about how your wall will look after this step.
+                                                </div>
+
+        </div>
+
+    </div>
+
+</section>
+</body>
+
+</html>
